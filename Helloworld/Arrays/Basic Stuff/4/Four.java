cb99b6e75756c12fe6d76fc28dfe04d4548bc1cc
hrrtjaymee <108507680+hrrtjaymee@users.noreply.github.com>
2024-01-03 13:18:01 +0800

    Create Four.java

---
+++ Helloworld/Arrays/Basic Stuff/4/Four.java
@@ -0,0 +1,33 @@
+/*Take 10 integer inputs from user and store them in an array.
+ * Now, copy all the elements in another array but in reverse order.
+ */
+import java.util.Scanner;
+
+public class Four {
+    public static void main(String[] args){
+        //num containers
+        int inputList[] = new int[10];
+        int reverseList[] = new int[10];
+
+        //initialize scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //getting user input
+        for (int i = 0; i < 10; i++ ){
+            System.out.println("Enter number: ");
+            inputList[i] = scanner.nextInt();
+        }
+
+        scanner.close();
+
+        //reversing inputList
+        int counter = 0;
+        for (int i = inputList.length - 1; i >= 0;  i--){
+            reverseList[counter] = inputList[i];
+            counter++;
+        }
+
+        System.out.println(inputList.toString());
+        System.out.println(reverseList.toString());
+    }
+}
