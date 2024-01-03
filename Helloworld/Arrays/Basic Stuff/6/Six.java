aff5a57adb7b62be33d2ae87192c09f16d30cff9
hrrtjaymee <108507680+hrrtjaymee@users.noreply.github.com>
2024-01-03 14:19:05 +0800

    Create Six.java

---
+++ Helloworld/Arrays/Basic Stuff/6/Six.java
@@ -0,0 +1,11 @@
+//import java.util.Arrays;
+
+public class Six {
+    public static void main(String[] args){
+        int a1[] = {3, 4, 5};
+        int a2[] = {5, 6, 7};
+        int[][] multidim = {a1, a2};
+
+        System.out.println(multidim[1][2]);
+    }
+}
