1a1e3d0be715cc2caeac8a85671eec1d678e0e46
HRT <108507680+hrrtjaymee@users.noreply.github.com>
2024-02-07 10:09:58 +0800

    Update Ch5_PrExercise1.java

--- skol/DSA/progExercise2/Ch5_PrExercise1.java
+++ skol/DSA/progExercise2/Ch5_PrExercise1.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-
 public class Ch5_PrExercise1{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        //taking user input
         System.out.println("Input a number: ");
         int input = sc.nextInt();
 
@@ -12,10 +12,10 @@ public class Ch5_PrExercise1{
         String digits = "";
 
         while (input > 0) {
-            int digit = input % 10;
-            sum += digit;
-            digits = digit + " " + digits;
-            input /= 10;
+            int digit = input % 10; //take the last digit of the umber
+            sum += digit; // adding to sum of digits
+            digits = digit + " " + digits; //storing last digit
+            input /= 10; //shortening the number
         }
 
         System.out.println("Individual digits: " + digits);
@@ -23,3 +23,4 @@ public class Ch5_PrExercise1{
 
         sc.close();
     }
+}
