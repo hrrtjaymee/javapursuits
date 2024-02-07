7109fd7dfd94c27ca89e5b9e2a300f2c354b2fe8
HRT <108507680+hrrtjaymee@users.noreply.github.com>
2024-02-07 10:40:37 +0800

    Create Exercise2.java

---
+++ skol/DSA/progExercise3/Exercise2.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void main(String[] args){
+        Scanner userInput = new Scanner(System.in);
+        int firstNum;
+        int secondNum;
+        String oddNums = "";
+        String allNums = "";
+        String squares = "";
+        int evenSum = 0;
+        int sumOddSquares = 0;
+        int i = firstNum;
+
+        
+        System.out.println("Enter first number: ");
+        firstNum = userInput.nextInt();
+
+        System.out.println("Enter second number: ");
+        secondNum = userInput.nextInt();
+
+        //firstNum must be less than secondNum
+        if(firstNum < secondNum){
+            while(i <= secondNum){
+                if(i%2 != 0){ //collect odd numbers
+                    oddNums = oddNums + " " + i;
+                    sumOddSquares += Math.pow(i, 2);
+                }
+                else{ //collect even numbers
+                    evenSum += i;
+                }
+                allNum = allNum + " " + i;
+                squares = squares + " " + Math.pow(i, 2);
+                i++;
+            }
+
+            System.out.println("Odd numbers: " + oddNums);
+            System.out.println("Sum of even numbers: " + oddNums);
+            System.out.println("Numbers: " + allNums);
+            System.out.println("Squares of the numberss: " + squares);
+            System.out.println("Squares of the odd numbers: " + sumOddSquares);
+        }
+        else{
+            System.out.println("First number must be less than the second number.");
+        }
+    }
+}
