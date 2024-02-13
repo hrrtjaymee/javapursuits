60e4ea89d086dafc5ccd31dd50cb0847d3971e21
HRT <108507680+hrrtjaymee@users.noreply.github.com>
2024-02-13 16:54:36 +0800

    Update takeData.py

--- skol/DSF/Basics of Data/takeData.py
+++ skol/DSF/Basics of Data/takeData.py
@@ -4,12 +4,16 @@ import os
 dirName = os.path.dirname(os.path.realpath(__file__)) #scours the location of the source code for data file
 path = dirName + "\cars.csv"
 with open(path, "r") as csvFile:
-  csvReader = csv.DictReader(csvFile)
+  csvReader = csv.DictReader(csvFile)  #csv.DictReader() different from csv.reader()
   cars = []
   for row in csvReader:
     cars.append(dict(row))
 print(cars)
 
 ##OUTPUT##
+#outputs a dictionary in a list
 # [{'Year': '1997', 'Make': 'Ford', 'Model': 'E350', 'Price': '3200.00'}, {'Year': '1999', 'Make': 'Chevy', 'Model': 'Venture', 'Price': '4800.00'}, 
 #  {'Year': '1996', 'Make': 'Jeep', 'Model': 'Grand Cherokee', 'Price': '4900.00'}]
+
+#Column header in csv file becomes dictionary key per instance of a "car"
+
