bb9d1e83b153ebaad4eb19d1aaa02723e9eae30d
HRT <108507680+hrrtjaymee@users.noreply.github.com>
2024-02-13 16:33:54 +0800

    Update takeData.py

--- skol/DSF/Basics of Data/takeData.py
+++ skol/DSF/Basics of Data/takeData.py
@@ -1,7 +1,7 @@
 import csv
 import os
 
-dirName = os.path.dirname(os.path.realpath(__file__)) #scourse the location of the source code for data file
+dirName = os.path.dirname(os.path.realpath(__file__)) #scours the location of the source code for data file
 path = dirName + "\cars.csv"
 with open(path, "r") as csvFile:
   csvReader = csv.DictReader(csvFile)
@@ -9,3 +9,7 @@ with open(path, "r") as csvFile:
   for row in csvReader:
     cars.append(dict(row))
 print(cars)
+
+##OUTPUT##
+# [{'Year': '1997', 'Make': 'Ford', 'Model': 'E350', 'Price': '3200.00'}, {'Year': '1999', 'Make': 'Chevy', 'Model': 'Venture', 'Price': '4800.00'}, 
+#  {'Year': '1996', 'Make': 'Jeep', 'Model': 'Grand Cherokee', 'Price': '4900.00'}]
