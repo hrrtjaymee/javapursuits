83ca83e5de503735239880aa64c6d179d2ba2110
HRT <108507680+hrrtjaymee@users.noreply.github.com>
2024-02-19 09:34:43 +0800

    Update androids.py
    from cp

--- skol/DSF/Basics of Data/androids.py
+++ skol/DSF/Basics of Data/androids.py
@@ -1,50 +1,33 @@
 import requests
-import json
-import os
 import csv
 
 params = {
     "q": "android",
-    "apiKey": "#",
+    "apiKey": "aa2a5ed2a07c4c4bae1bff8594b52794",
     "pageSize": "20"
-    }
-req = requests.get("https://newsapi.org/v2/everything", params)
-articles = json.loads(req.text)
-#print(articles)
-myList = articles["articles"]
-fields  = ["source", "author", "title", "url", "publishedAt"]
-sources = []
-authors = []
-titles = []
-urls = []
-publishedAt = []
+}
 
-for i in range(0, len(myList)-1):
-    sources.append(myList[i]["source"]["name"])
-    authors.append(myList[i]["author"])
-    titles.append(myList[i]["title"])
-    urls.append(myList[i]["url"])
-    publishedAt.append(myList[i]["publishedAt"])
+response = requests.get("https://newsapi.org/v2/everything", params)
+articles = response.json()["articles"]
 
-##authors #get rid of email addresses
-for i in range(len(authors)):
-    if "(" in authors[i]:
-        first = authors[i].index("(") + 1
-        second = authors[i].index(")")
-        authors[i]= authors[i][first : second]
-#formatting date
-for i in range(len(publishedAt)):
-    publishedAt[i] = publishedAt[i][0:10]
+cleaned_articles = []
 
-print(publishedAt)
-    
+for article in articles:
+    source = article["source"]["name"]
+    author = article["author"]
+    title = article["title"]
+    url = article["url"]
+    published_at = article["publishedAt"][0:10]
+
+    # Clean author name
+    if "(" in author:
+        first = author.index("(") + 1
+        second = author.index(")")
+        author = author[first:second]
+
+    cleaned_articles.append([source, author, title, url, published_at])
+
 with open("articles.csv", "w", newline="") as csvFile:
-    fieldNames = ["Source", "Author", "Title", "URL", "PublishedAt"]
-    writer = csv.DictWriter(csvFile, fieldnames = fieldNames)
-
-    writer.writeheader()
-    for i in range(len(myList)-1):
-        writer.writerow({fieldNames[0]:sources[i], fieldNames[1]:authors[i], fieldNames[2]:titles[i], 
-                         fieldNames[3]:urls[i], fieldNames[4]:publishedAt[i]})
-
-    
+    writer = csv.writer(csvFile)
+    writer.writerow(["Source", "Author", "Title", "URL", "PublishedAt"])
+    writer.writerows(cleaned_articles)
